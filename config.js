c10e597e32f57c42c0c4b27f89e52d51ccb6acd4
feldyb <68964118+feldyb@users.noreply.github.com>
2025-06-30 00:38:06 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
-// config.js
+
 const WEATHER_API_KEY = '9cb09ff271e34d97a9aad23bd94066e8';
 const DEFAULT_CITY = 'Bucuresti';
-const UNITS = 'metric'; // metric sau imperial
+const UNITS = 'metric'; // Poți schimba în 'imperial' dacă vrei °F și mile/h
+
